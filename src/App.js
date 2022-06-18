e812e0a924a627d691b3959d458b37d71babeb4c
mdave94 <molnardavid2002@gmail.com>
2022-06-18 14:13:48 +0200

    MainScreen photo added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
+import MainScreen from './screens/MainScreen/MainScreen';
 
 function App() {
   return (
     <div className='App'>
       <NavBar />
+      <MainScreen/>
     </div>
   );
 }
