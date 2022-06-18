62b3ca433a9cdf795178f546eaee11f19846c836
mdave94 <molnardavid2002@gmail.com>
2022-06-18 13:01:26 +0200

    navbar update

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,7 +1,38 @@
 import React from 'react';
+import "./NavBar.css"
+
 
 const NavBar = () => {
-  return <div>NavBar</div>;
+  return(
+  <div className='navbar' >
+    <div className='navbar-content-left'>
+      <div className='navbar-logo'>
+        IMG
+      </div>
+      <div className='navbar-menu'>
+          <ul>
+              <li>Logo</li>
+              <li>Főoldal</li>
+              <li>Rólunk</li>
+              <li>Munkáink</li>
+          </ul>
+      </div>
+    </div>
+    <div className='navbar-content-right'>
+      <div className='lang-selector'>
+        HUN
+      </div>
+      <div className='contact'>
+        Kapcsolat
+      </div>
+    </div>
+    
+  </div>
+
+  )
+  
+
+
 };
 
 export default NavBar;
