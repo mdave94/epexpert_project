614726ce2b4b7a33e4f7690a5e47a2984fcb2068
Kovács Krisztián <kkovax94@gmail.com>
2022-06-18 13:40:42 +0200

    Updated

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,38 +1,29 @@
 import React from 'react';
-import "./NavBar.css"
-import logo  from '../../images/logo.png'
+import './NavBar.css';
+import logo from '../../images/logo.png';
 
 const NavBar = () => {
-  return(
-  <div className='navbar' >
-    <div className='navbar-content-left'>
-      <div className='navbar-logo'>
-        <img className='navbar-logo-img' src={logo} />
-      </div>
-      <div className='navbar-menu'>
+  return (
+    <div className='navbar'>
+      <div className='navbar-content-left'>
+        <div className='navbar-logo'>
+          <img className='navbar-logo-img' alt='logo' src={logo} />
+        </div>
+        <div className='navbar-menu'>
           <ul className='navbar-menu-list'>
-              <li>Logo</li>
-              <li>Főoldal</li>
-              <li>Rólunk</li>
-              <li>Munkáink</li>
+            <li>Logo</li>
+            <li>Főoldal</li>
+            <li>Rólunk</li>
+            <li>Munkáink</li>
           </ul>
+        </div>
+      </div>
+      <div className='navbar-content-right'>
+        <div className='lang-selector'>HUN</div>
+        <div className='contact-btn'>Kapcsolat</div>
       </div>
     </div>
-    <div className='navbar-content-right'>
-      <div className='lang-selector'>
-        HUN
-      </div>
-      <div className='contact-btn'>
-        Kapcsolat
-      </div>
-    </div>
-    
-  </div>
-
-  )
-  
-
-
+  );
 };
 
 export default NavBar;
