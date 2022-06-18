c287b98678ca5f7966bfc395662642b559badffc
mdave94 <molnardavid2002@gmail.com>
2022-06-18 14:13:48 +0200

    MainScreen photo added

---
+++ src/screens/MainScreen/MainScreen.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './MainScreen.css'
+
+
+const MainScreen = () => {
+  return (
+    <div className='mainscreen-container'>
+      <div className='mainscreen-picture-container'>
+
+      </div>
+    </div>
+  )
+}
+
+export default MainScreen
